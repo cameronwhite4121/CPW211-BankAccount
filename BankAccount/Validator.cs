141f47c7d42d5555c8e6a39e58b902b9c826f6d1
Cameron White <cameronwhite4121@gmail.com>
2024-08-12 00:29:35 -0700

    Made validator class, with tests for the new isWithionRange method

---
+++ BankAccount/Validator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public static class Validator
+    {
+        public static bool isWithinRange(double minimumValue, double maxValue, double valueToTest)
+        {
+            if (valueToTest >= minimumValue && valueToTest <= maxValue)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
